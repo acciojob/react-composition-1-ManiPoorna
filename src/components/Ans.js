a127bd5007d7feea95a99d4329a6482d1eb1809a
Mani Poorna <ndmprao2001@gmail.com>
2023-08-15 21:38:07 +0530

    I Think done

--- src/components/Ans.js
+++ src/components/Ans.js
@@ -1,15 +1,25 @@
 import React, {useState} from "react";
+import ReactDOM from "react-dom";
+
 
 
 const Ans = () => {
+    let [tabClick,setTabClick] = useState("");
 
+    function display(e){
+        console.log(e.target.innerText)
+        setTabClick(e.target.innerText);
+    }
     return(
         <div>
             <ul>
-                <li onClick={(e)=>{<p>This is the content for {e.target.value}</p>}}>Tab 1</li>
-                <li onClick={(e)=>{<p>This is the content for {e.target.value}</p>}}>Tab 2</li>
-                <li onClick={(e)=>{<p>This is the content for {e.target.value}</p>}}>Tab 3</li>
+                <li onClick={(e)=>display(e)}>Tab 1</li>
+                <li onClick={(e)=>display(e)}>Tab 2</li>
+                <li onClick={(e)=>display(e)}>Tab 3</li>
             </ul>
+            {
+                tabClick && <p>This is the content for {tabClick}.</p>
+            }
         </div>
     )
 }
